44db9804b3aebf8c25a8a9f6fe6fd299111d498f
Arkaprabha Banerjee <124279312+Arkaprabha13@users.noreply.github.com>
2024-01-26 12:05:32 +0530

    Create email_checker.py

---
+++ login_page/email_checker.py
@@ -0,0 +1,7 @@
+import re
+def is_valid_email(email):
+    email_pattern = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+    if re.match(email_pattern, email):
+        return True, "Email address is valid."
+    else:
+        return False, "Invalid email address."
